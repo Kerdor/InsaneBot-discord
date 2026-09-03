1732f1b1107cd3057cfe53e58aad43622fdfa50c
Nikita <59965225+Kerdor@users.noreply.github.com>
2026-09-03 04:14:55 +0300

    Fix Vite Activity host validation

--- activities/client/vite.config.js
+++ activities/client/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
     server: {
+        allowedHosts: true,
         proxy: {
             "/api": "http://127.0.0.1:8080",
         },
